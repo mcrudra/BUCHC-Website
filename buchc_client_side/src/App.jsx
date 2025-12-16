a0a2c417cc08aaaac9eea0da8700f04637da84dd
shimul47 <oshimul54@gmail.com>
2025-12-16 13:48:32 +0600

    Development Logo

--- buchc_client_side/src/App.jsx
+++ buchc_client_side/src/App.jsx
@@ -1,15 +1,19 @@
-// App.jsx (main component)
+// App.jsx
 import React from "react";
 import Navbar from "./Navbar";
 import Hero from "./components/Hero";
 import UpcomingEvents from "./components/UpcomingEvents";
+import OurTeam from "./components/OurTeam";
+import Development from "./components/Development";
 
 function App() {
   return (
-    <div className="w-full">
+    <div className="w-full relative">
+      <Development />
       <Navbar />
       <Hero />
       <UpcomingEvents />
+      <OurTeam />
     </div>
   );
 }
---
+++ buchc_client_side/src/components/OurTeam.jsx
@@ -0,0 +1,9 @@
+export default function OurTeam() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-gray-800">
+        Our Team Page Coming Soon!
+      </h1>
+    </div>
+  );
+}
