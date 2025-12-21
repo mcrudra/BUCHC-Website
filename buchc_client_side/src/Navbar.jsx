56ce5aef983f88501acf0a63ddf47a18b32a9395
Monish Chanda Rudra <immcrudra@yahoo.com>
2025-12-22 04:48:31 +0600

    Final

--- buchc_client_side/src/Navbar.jsx
+++ buchc_client_side/src/Navbar.jsx
@@ -38,7 +38,7 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="w-full bg-white shadow-lg">
+    <nav className="sticky top-0 z-50 w-full bg-white shadow-lg">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
